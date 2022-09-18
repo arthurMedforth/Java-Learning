bc422d9fe558c8d904c75d6487bdb87389942fcc
desperateNovice <113832644+desperateNovice@users.noreply.github.com>
2022-09-18 12:03:08 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+
+    int age;
+
+    public HelloWorld(int age){
+        this.age = age;
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        HelloWorld newHelloWorld = new HelloWorld(22);
+        System.out.println(newHelloWorld.age);
+    }
+
+}
