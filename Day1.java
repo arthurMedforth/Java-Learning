75f10cdd7d2c8778b5209c10b135807c729fc230
Arthur <medfortha@gmail.com>
2022-09-26 16:59:41 +0100

    Advent of code day 1

---
+++ Day1.java
@@ -0,0 +1,71 @@
+import java.io.File;  // Import the File class
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class Day1 {
+  public static void main(String[] args) {
+
+    int count = 0;
+
+    int increasedCount = 0;
+
+    try {
+      // create a new file object
+      File file = new File("inpudt.txt");
+
+      // create an object of Scanner
+      // associated with the file
+      Scanner sc = new Scanner(file);
+
+      // read each line and
+      // count number of lines
+      while(sc.hasNextLine()) {
+
+        sc.nextLine();
+
+        count++;
+      }
+
+      int [] intArray = new int [count];
+
+      Scanner sc2 = new Scanner(file);
+
+      int i = 0;
+
+      while(sc2.hasNextInt())
+      {
+
+        intArray[i++] = sc2.nextInt();
+
+      }
+
+      int currentSum = 0;
+
+      int newSum = 0;
+
+      for (int j = 0; j<intArray.length-2; j++){
+
+        newSum = intArray[j]+intArray[j+1]+intArray[j+2];
+
+        if (newSum>currentSum && j!=0){
+
+          increasedCount++;
+
+        }
+
+        currentSum = newSum;
+
+      }
+
+      // close scanners
+      sc.close();
+
+      sc2.close();
+
+      System.out.println(increasedCount);
+
+    } catch (Exception e) {
+
+      e.printStackTrace();
+    }
+  }
+}
